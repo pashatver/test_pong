38654fdc1918b80d2798820fcb8efa73c04a1051
pashatver <pashatver@mail.ru>
2026-06-13 02:15:06 +0300

    last commt2

--- test-without-for.c
+++ test-without-for.c
@@ -101,46 +101,40 @@ static void draw(void) {
     printf("\033[2J\033[H");
 
     y = 0;
-    while (y < field_heigth) {
+    while (y < field_height) {
         x = 0;
-        while (x < field_weidth) {
+        while (x < field_width) {
             symbol = ' ';
-            if (y == 0 || y == field_heigth - 1) {
-                if (y == 0 && x == field_weidth / 2 - 5) {
+            if (y == 0 || y == field_height - 1) {
+                if (y == 0 && x == field_width / 2 - 5) {
                     symbol = '0' + player1_score / 10;
-                } else if (y == 0 && x == field_weidth / 2 - 4) {
+                } else if (y == 0 && x == field_width / 2 - 4) {
                     symbol = '0' + player1_score % 10;
-                } else if (y == 0 && x == field_weidth / 2 - 3) {
+                } else if (y == 0 && x == field_width / 2 - 3) {
                     symbol = ':';
-                } else if (y == 0 && x == field_weidth / 2 - 2) {
+                } else if (y == 0 && x == field_width / 2 - 2) {
                     symbol = '0' + player2_score / 10;
-                } else if (y == 0 && x == field_weidth / 2 - 1) {
+                } else if (y == 0 && x == field_width / 2 - 1) {
                     symbol = '0' + player2_score % 10;
                 } else {
                     symbol = '-';
                 }
-            } else if (x == 0 || x == field_weidth - 1) {
+            } else if (x == 0 || x == field_width - 1) {
                 symbol = '|';
             } else if (x == ball_x && y == ball_y) {
                 symbol = 'O';
             } else {
                 i = 0;
-                while (i < paddle_size) {
-                    if (x == 1 && y == left_paddle_y + i) {
+                while (i < paddle_size && symbol == ' ') {
+                    if (x == 1 && y == left_paddle_y + i)
                         symbol = '#';
-                        break;
-                    }
                     i++;
                 }
-                if (symbol == ' ') {
-                    i = 0;
-                    while (i < paddle_size) {
-                        if (x == field_weidth - 2 && y == right_paddle_y + i) {
-                            symbol = '#';
-                            break;
-                        }
-                        i++;
-                    }
+                i = 0;
+                while (i < paddle_size && symbol == ' ') {
+                    if (x == field_width - 2 && y == right_paddle_y + i)
+                        symbol = '#';
+                    i++;
                 }
             }
             putchar(symbol);
